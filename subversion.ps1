a37d7d0456a2f80c53c85159018f81382e2236dc
MatteoSp <mspreafico@gmail.com>
2015-05-07 14:11:46 +0200

    A powershell version of the SVN post-commit handler

---
+++ subversion.ps1
@@ -0,0 +1,39 @@
+# Requires Powershell 3.0
+
+Param(
+  [string]$svnPath,
+  [string]$revision,
+  [string]$repoName
+)
+
+$domain = "YOUR_DOMAIN.slack.com"
+$token = "TOKEN"
+$endpoint = "https://${domain}/services/hooks/subversion?token=${token}"
+#$revisionUrlBase = "WEB_VIEW_BASE URL" #if any
+
+$log = (svnlook log -r $revision $svnPath)
+$who = (svnlook author -r $revision $svnPath)
+$changes = (svnlook changed -r $revision $svnPath)
+
+$payload = @{   
+    attachments = @(@{
+        pretext = "Commit completed: $repoName rev. $revision"
+        text = "Message: $log"
+        fallback = "Commit completed: $repoName rev. $revision"
+        title = "Commit details"
+        # title_link = "$revisionUrlBase$revision"
+        color = '#439FE0'
+        fields = @(@{
+            title = "Changes"
+            value = $changes
+            short = $false
+        }, @{
+            title = "Author"
+            value = $who
+            short = $true
+        })
+    })
+}
+
+$json = (ConvertTo-Json $payload -Depth 99)
+Invoke-RestMethod -Uri $endpoint -Method Post -ContentType "application/json" -Body $json
